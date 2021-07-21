59865940392eaa7e902869d36d1a47f9ad5d332d
LuizDaniloRibeiro <luizdanilor@gmail.com>
2021-07-21 20:37:34 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-sgan1r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-sgan1r)
---
+++ src/app/toolbar-basic-example.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic toolbar
+ */
+@Component({
+  selector: 'toolbar-basic-example',
+  templateUrl: 'toolbar-basic-example.html',
+  styleUrls: ['toolbar-basic-example.css'],
+})
+export class ToolbarBasicExample {}
+
+
+/**  Copyright 2021 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
